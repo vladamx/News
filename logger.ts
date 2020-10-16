45add43d9d00fddc37a1ba547d0e8c27bdee4289
Vladimir Milojevic <vladimir.milojevic@p3-group.com>
2020-10-16 23:03:42 +0200

    Add simple logger

---
+++ logger.ts
@@ -0,0 +1,11 @@
+export const log = {
+  debug(message: string) {
+    console.log(`DEBUG: ${message}`);
+  },
+  error(message: string) {
+    console.log(`ERROR: ${message}`);
+  },
+  info(message: string) {
+    console.log(`INFO: ${message}`);
+  },
+};
