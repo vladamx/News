3e760690abfb79641f973c4db83687c21dea8555
Vladimir Milojevic <vladimir.milojevic@p3-group.com>
2020-10-18 15:13:48 +0200

    Make category form optional for useNewsArticle hook

--- news/useNewsArticles.ts
+++ news/useNewsArticles.ts
@@ -3,20 +3,17 @@ import { newsService } from './newsService';
 import { NewsArticles } from './newsArticle';
 import { CategoryForm } from './categories/categoryForm';
 
-export const useNewsArticles = (
-  country: string,
-  category: CategoryForm = { name: '' },
-) => {
+export const useNewsArticles = (country: string, category?: CategoryForm) => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<NewsArticles | undefined>();
   const [error, setError] = useState<NewsError | undefined>();
   useEffect(() => {
     let mounted = true;
     const request = () => {
-      if (category.limit && category.name) {
+      if (category?.limit && category?.name) {
         return newsService.getAllNewsArticlesByCategory(country, category.name);
       }
-      if (category.name) {
+      if (category?.name) {
         return newsService.getTopNewsArticlesByCategory(country, category.name);
       }
       return newsService.getAllTopNewsArticles(country);
